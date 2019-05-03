51c77a25693377a37cd6a8646b6030b2914f0b55
Youness <Youness@DESKTOP-BQ87NOV>
2019-05-03 14:22:31 +0200

    Ejer 5. creo los métodos leer y escribir en la clase Profesores

--- src/main/java/org/iesalandalus/programacion/reservasaulas/modelo/dao/Profesores.java
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/modelo/dao/Profesores.java
@@ -5,6 +5,14 @@
  */
 package org.iesalandalus.programacion.reservasaulas.modelo.dao;
 
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 import javax.naming.OperationNotSupportedException;
@@ -15,6 +23,7 @@ import org.iesalandalus.programacion.reservasaulas.modelo.dominio.Profesor;
  * @author Youness
  */
 public class Profesores {
+    private static final String NOMBRE_FICHERO_PROFESORES = "ficheros/profesores.dat";
     private List<Profesor> coleccionProfesores;
   
   public Profesores(){
@@ -107,5 +116,40 @@ public class Profesores {
       
       return profesoresRepresentados;
   }
+  
+  public void leer() throws FileNotFoundException, IOException {
+        File ficheroProfesores = new File(NOMBRE_FICHERO_PROFESORES);
+        try (ObjectInputStream entrada = new ObjectInputStream(new FileInputStream(ficheroProfesores))) {
+            Profesor profesor = null;
+            do {
+                profesor = (Profesor) entrada.readObject();
+                insertar(profesor);
+            } while (profesor != null);
+        } catch (ClassNotFoundException e) {
+            System.out.println("No puedo encontrar la clase que tengo que leer.");
+        } catch (FileNotFoundException e) {
+            System.out.println("No puedo abrir el fihero de profesores.");
+        } catch (EOFException e) {
+            System.out.println("Fichero profesores leído satisfactoriamente.");
+        } catch (IOException e) {
+            System.out.println("Error inesperado de Entrada/Salida.");
+        } catch (OperationNotSupportedException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void escribir() {
+        File ficheroProfesores = new File(NOMBRE_FICHERO_PROFESORES);
+        try (ObjectOutputStream salida = new ObjectOutputStream(new FileOutputStream(ficheroProfesores))) {
+            for (Profesor profesor : coleccionProfesores) {
+                salida.writeObject(profesor);
+            }
+            System.out.println("Fichero aulas escrito satisfactoriamente.");
+        } catch (FileNotFoundException e) {
+            System.out.println("No puedo crear el fichero de profesores");
+        } catch (IOException e) {
+            System.out.println("Error inesperado de Entrada/Salida");
+        }
+    }
     
 }
